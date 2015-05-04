47321cec53822d514e152b46d4ff041522355bd8
Bart van de Biezen <bart@bartvandebiezen.com>
2015-05-04 13:20:22 +0200

    Fixed no URLs bug and small optimizations
    When there were no URLs in the critical CSS, the plugin kept compiling
    the SCSS.

--- snippets/scss.critical.php
+++ snippets/scss.critical.php
@@ -5,63 +5,94 @@
  * @author    Bart van de Biezen <bart@bartvandebiezen.com>
  * @link      https://github.com/bartvandebiezen/kirby-v2-scssphp
  * @return    CSS
- * @version   0.8
+ * @version   0.8.5
  */
 
-// Using 'realpath' seems to work best in different situations.
+// Using realpath seems to work best in different situations.
 $root = realpath(__DIR__ . '/../..');
 
-// For checking and creating absolute paths in your inlined CSS.
+// Get site URL. Used for checking and creating absolute paths in critical CSS.
 $siteUrl = $site->url();
 
-// For checking non critical SCSS file and checking and creating critical SCSS file for the current template.
-$templateName       = $page->template();
-$templateSourceFile = $root . '/assets/scss/' . $templateName . '.critical.scss';
+// Set file paths. Used for checking non critical SCSS and checking and compiling critical CSS for current template.
+$template     = $page->template();
+$criticalSCSS = $root . '/assets/scss/' . $template . '.critical.scss';
+$criticalCSS  = $root . '/assets/css/'  . $template . '.critical.css';
+$templateSCSS = $root . '/assets/scss/' . $template . '.scss';
+$defaultSCSS  = $root . '/assets/scss/default.scss';
 
-if ( $templateName != 'default' and file_exists($templateSourceFile) ) {
-	$sourceFile            = $templateSourceFile;
-	$compiledFile          = $root . '/assets/css/' . $templateName . '.critical.css';
-	$nonCriticalSourceFile = $root . '/assets/scss/' . $templateName . '.scss';
-} else {
-	$sourceFile            = $root . '/assets/scss/default.critical.scss';
-	$compiledFile          = $root . '/assets/css/default.critical.css';
-	$nonCriticalSourceFile = $root . '/assets/scss/default.scss';
+// Check if there is a critical SCSS. If not, use default. If template is default, skip check.
+if ($template == 'default' or !file_exists($criticalSCSS)) {
+	$criticalSCSS = $root . '/assets/scss/default.critical.scss';
+	$criticalCSS  = $root . '/assets/css/default.critical.css';
 }
 
-// Compile when: (1) the CSS file doesn't exist; (2) the non critical source file is updated ;(3) the critical source file is updated; (4) Absolute URLs from the current server cannot be found.
-if ( !file_exists($compiledFile) or filemtime($nonCriticalSourceFile) > filemtime($compiledFile) or filemtime($sourceFile) > filemtime($compiledFile) or !strpos(file_get_contents($compiledFile), $siteUrl) ) {
+// Check if there is a non critical template SCSS. If not, use default. If template is default, skip check.
+if ($template == 'default' or !file_exists($templateSCSS)) {
+	$templateSCSS = $defaultSCSS;
+}
+
+// Set variable for checking if critical CSS needs to be compiled. I use a variable instead of combined if statement to reduce the number of times PHP needs to access different files.
+$needsCompiling = false;
+
+// Compile critical SCSS when critical CSS doesn't exists.
+if (!file_exists($criticalCSS)) {
+	$needsCompiling = true;
+} else {
+	// Compile when template or default non critical SCSS is newer than critical CSS.
+	$templateSCSSFileTime = filemtime($templateSCSS);
+	$defaultSCSSFileTime = filemtime($defaultSCSS);
+	$criticalCSSFileTime = filemtime($criticalCSS);
+	if ($templateSCSSFileTime > $criticalCSSFileTime or $defaultSCSSFileTime > $criticalCSSFileTime) {
+		$needsCompiling = true;
+	} else {
+		// Compile when critical SCSS is newer than critical CSS.
+		$criticalSCSSFileTime = filemtime($criticalSCSS);
+		if ($criticalSCSSFileTime > $criticalCSSFileTime) {
+			$needsCompiling = true;
+		} else {
+			// Compile when critical CSS contains incorrect URLs, but only when it contains URLs.
+			$criticalCSSContents = file_get_contents($criticalCSS);
+			if (strpos($criticalCSSContents, 'url') and !strpos($criticalCSSContents, $siteUrl)) {
+				$needsCompiling = true;
+			}
+		}
+	}
+}
+
+if ($needsCompiling) {
 
 	// Activate library.
 	require_once $root . '/site/plugins/scssphp/scss.inc.php';
 	$parser = new scssc();
 
-	// Setting compression provided by library.
+	// Set compression provided by library.
 	$parser->setFormatter('scss_formatter_compressed');
 
-	// Setting relative @import paths.
+	// Set relative @import paths.
 	$importPath = $root . '/assets/scss';
 	$parser->addImportPath($importPath);
 
-	// Place SCSS file in buffer.
-	$buffer = file_get_contents($sourceFile);
+	// Put SCSS in buffer.
+	$buffer = file_get_contents($criticalSCSS);
 
-	// Compile content in buffer.
+	// Compile SCSS and place CSS in buffer.
 	$buffer = $parser->compile($buffer);
 
-	// Minify the CSS even further.
+	// Minify CSS even further.
 	require_once $root . '/site/plugins/scssphp/minify.php';
 	$buffer = minifyCSS($buffer);
 
-	// Make relative URLs absolute
-	// CRED: <http://stackoverflow.com/questions/9798378/preg-replace-regex-to-match-relative-url-paths-in-css-files>
+	// Make relative URLs absolute.
+	// CRED: <http://stackoverflow.com/questions/9798378/preg-replace-regex-to-match-relative-url-paths-in-css-files>.
 	$compiledPath = $siteUrl . '/assets/css/';
 	$relativePath = '#url\((?!\s*[\'"]?(?:https?:)?//)\s*([\'"])?#';
 	$absolutePath = "url($1{$compiledPath}";
 	$buffer = preg_replace($relativePath, $absolutePath, $buffer);
 
-	// Update critical CSS file.
-	file_put_contents($compiledFile, $buffer);
+	// Update critical CSS.
+	file_put_contents($criticalCSS, $buffer);
 }
 
 ?>
-<style type="text/css"><?php echo file_get_contents($compiledFile); ?> </style>
+<style type="text/css"><?php echo file_get_contents($criticalCSS); ?></style>
